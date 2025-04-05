973fda4c9deae48a697f278573f416c549833159
ricelgiannastasio <ricelgiannastasio@gmail.com>
2025-04-05 06:11:58 +0000

    Add 24.ts

---
+++ src/24.ts
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+let randomNumber = getRandomInt(10); // Random number between 0 and 9
+console.log(randomNumber);
